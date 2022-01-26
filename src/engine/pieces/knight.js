8f4dcf8460edaeb7edc98063af54e2007c1b2f52
Alison Thomson <Alison.Thomson@softwire.com>
2022-01-26 18:01:28 +0000

    Knight still not working but closer!

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -14,23 +14,30 @@ export default class Knight extends Piece {
         let rowStartPoint = location.row-2;
         let colStartPoint = location.col-1;
 
-        for (let i=0; i<=1; i++) {
+        //There are 4 rows of squares
+        for (let i=0; i<=3; i++) {
 
-            if (location.row !== 0) {
+            //if (location.row !== 0) {
+            //There are 2 column values for each row value
+            for (let j=0; j<=1; j++) { 
 
-                for (let j=0; j<=2; j++) { 
-
-                    if (location.row !==0) {
-                    
-                        if (j!==1) {
-                            
-                            availableMoves.push(Square.at(rowStartPoint + i, colStartPoint + j));
-                        }
-                }  
-            console.log("Row #1" + availableMoves);
+                if ((i=== 0 || i===3) && j===1) {
+                    //on rows #1&4 the column increases by 2
+                    colStartPoint = colStartPoint +1;
+                } else if ((i=== 1 || i===2) && j===1) {
+                    //on rows #2&3 the column first needs to be decreased by 1
+                    colStartPoint = colStartPoint -1;
+                } else if ((i=== 1 || i===2) && j===1) {
+                    //on rows #2&3 the column increases by 4
+                    colStartPoint = colStartPoint +4;
+                }
+                availableMoves.push(Square.at(rowStartPoint + i, colStartPoint + j));
+               
+            
         }
-    }
-}
+   }
+   console.log(availableMoves);
+   // we need to remove any squares that fall outside the board (<0 or >7)
     return availableMoves; 
 }
 }
